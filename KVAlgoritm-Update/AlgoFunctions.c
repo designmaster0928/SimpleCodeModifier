0db763caf89e3d878abb5dabd9c98d6766329a03
designmaster0928 <norrisdavis0928@gmail.com>
2023-07-31 18:20:17 -0700

    first step

---
+++ KVAlgoritm-Update/AlgoFunctions.c
@@ -0,0 +1,196 @@
+// Source file for Algoritm functions
+// Shared with ESP32 code
+#include "AlgoFunctions.h"
+
+int test() {
+	return 5;
+}
+
+void KVtargetAlgoritm(double KVval, long timestamp) {
+	// KV algoritm
+	// Takes in current measured KV point and timestamp for that point
+	// Stores data in KV struct array, and calculates LOW, HIGH and TARGET
+	// First sample [0] is newest sample. [400.000] is oldest  
+
+	int i;
+	double average;
+
+
+	// Save KV value
+	KValgo[timestamp].kv_UFH = KVval;
+
+	// Calculate KV average
+	average = 0;
+	for (i = 0; i < KV_MEAS_AVGSAMPLES; i++) {
+		average += KValgo[timestamp].kv_UFH;
+	}
+	average = average / (double)KV_MEAS_AVGSAMPLES;
+	KValgo[timestamp].kv_mean_UFH = average;
+
+	// Calculate envelope
+	// Check if envelope is broken, then update envelope
+	if (KValgo[timestamp].kv_UFH > KValgo[timestamp].kv_high_UFH) {
+		// Update high value
+		KValgo[timestamp].kv_high_UFH = KValgo[timestamp].kv_UFH;
+	}
+	if (KValgo[timestamp].kv_UFH < KValgo[timestamp].kv_low_UFH) {
+		// Update low value
+		KValgo[timestamp].kv_low_UFH = KValgo[timestamp].kv_UFH;
+	}
+	// Decay envelope
+	// Linear regression over 480 samples, from high to mean value
+	// high regression
+	KValgo[timestamp].kv_high_UFH -= ((KValgo[timestamp].kv_high_UFH - KValgo[timestamp].kv_mean_UFH) / (double)KV_ENVELOPE_DECAY);
+	// low regression
+	KValgo[timestamp].kv_low_UFH += ((KValgo[timestamp].kv_mean_UFH - KValgo[timestamp].kv_low_UFH) / (double)KV_ENVELOPE_DECAY);
+
+	// Set target (percentage between KV_high and KV_mean)
+	KValgo[timestamp].kv_target_UFH = KValgo[timestamp].kv_mean_UFH + ((KValgo[timestamp].kv_high_UFH - KValgo[timestamp].kv_mean_UFH) * (double)KV_AMBITION_UFH);
+
+
+
+}
+
+void LoadDefaultHeatCurve(void) {
+	// Deault heat curve is loaded into HouseCurveArray
+	// Curve extends from -20 to 30 degrees ambient
+	// For each point 20 samples are summed. Adding new value then has an effect of 5% on the stored value
+#define W_PER_M2 0.063
+#define M2 150
+#define DELTA -20
+
+//#define HouseAmbMaxDelta 30 - -20;
+#define HOUSEAMBMIN -20
+#define HOUSEAMBMAX 30
+#define HOUSEAMBRANGEDELTA HOUSEAMBMAX - HOUSEAMBMIN
+
+
+	double MaxPoweratminus15;	// Max power for house at -15 ambient
+	// At - 15, delta is 35. Poff = a * Tout P = 0. At 0 degrees (-20 delta), DeltaTemp is 20
+	MaxPoweratminus15 = M2 * W_PER_M2;
+	HouseModel.AlphaDefault = MaxPoweratminus15 / 35.0;
+	HouseModel.BetaDefault = (double)DELTA * HouseModel.AlphaDefault;
+
+
+	int i;
+	double AmbTemp; //	Variable for the x axis, ambient temperature
+	double Phouse;	// House estimated power
+
+	for (i = 0; i < HOUSE_ARRAY_SIZE; i++) {
+		AmbTemp = ((double)i / (double)HOUSE_ARRAY_SIZE) * (double)HOUSEAMBRANGEDELTA;
+		Phouse = HouseModel.AlphaDefault * AmbTemp + HouseModel.BetaDefault;
+		HouseArray[i] = Phouse;
+		HouseArrayx[i] = (double)i; // Just for regression
+
+	}
+
+	// Set Modifier shift
+	HouseModel.BetaMod = 0;
+}
+
+#define SIMPLE_LINEAR_REGRESSION_ERROR_INPUT_VALUE -2
+#define SIMPLE_LINEAR_REGRESSION_ERROR_NUMERIC     -3
+
+int simple_linear_regression(const double* x, const double* y, const int n, double* slope_out, double* intercept_out, double* r2_out) {
+	double sum_x = 0.0;
+	double sum_xx = 0.0;
+	double sum_xy = 0.0;
+	double sum_y = 0.0;
+	double sum_yy = 0.0;
+	double n_real = (double)(n);
+	int i = 0;
+	double slope = 0.0;
+	double denominator = 0.0;
+
+	if (x == NULL || y == NULL || n < 2) {
+		return SIMPLE_LINEAR_REGRESSION_ERROR_INPUT_VALUE;
+	}
+
+	for (i = 0; i < n; ++i) {
+		sum_x += x[i];
+		sum_xx += x[i] * x[i];
+		sum_xy += x[i] * y[i];
+		sum_y += y[i];
+		sum_yy += y[i] * y[i];
+	}
+
+	denominator = n_real * sum_xx - sum_x * sum_x;
+	if (denominator == 0.0) {
+		return SIMPLE_LINEAR_REGRESSION_ERROR_NUMERIC;
+	}
+	slope = (n_real * sum_xy - sum_x * sum_y) / denominator;
+
+	if (slope_out != NULL) {
+		*slope_out = slope;
+	}
+
+	if (intercept_out != NULL) {
+		*intercept_out = (sum_y - slope * sum_x) / n_real;
+	}
+
+	if (r2_out != NULL) {
+		denominator = ((n_real * sum_xx) - (sum_x * sum_x)) * ((n_real * sum_yy) - (sum_y * sum_y));
+		if (denominator == 0.0) {
+			return SIMPLE_LINEAR_REGRESSION_ERROR_NUMERIC;
+		}
+		*r2_out = ((n_real * sum_xy) - (sum_x * sum_y)) * ((n_real * sum_xy) - (sum_x * sum_y)) / denominator;
+	}
+
+	return 0;
+}
+
+
+
+void UpdateHeatCurve(double AmbTemp, double HPPower) {
+	// Function takes in current KVData and ambient temperature, updates house model
+	// Then updates fitted house curve
+	// Find closest match to index
+#define HOUSESAMPLESPERPOINT 20 // 20 samples per point. Averaged when adding new
+
+	int index;
+
+	index = round((double)HOUSE_ARRAY_SIZE * ((AmbTemp - (double)HOUSEAMBMIN) / (double)HOUSEAMBRANGEDELTA));
+	// New addition only has 5% impact (1/20)
+	HouseArray[index] = HouseArray[index] * (1.0 - (1.0 / (double)HOUSESAMPLESPERPOINT)) + HPPower / (double)HOUSESAMPLESPERPOINT;
+
+	// Fit house curve
+	// Use center mass function to create new Ax+B (https://www.bragitoff.com/2018/05/linear-fitting-c-program/)
+	// https://stackoverflow.com/questions/5083465/fast-efficient-least-squares-fit-algorithm-in-c
+	HouseModel.Alpha = HouseModel.AlphaDefault; // for now, use default
+	HouseModel.Beta = HouseModel.BetaDefault;
+
+	double slope = 0.0;
+	double intercept = 0.0;
+	double r2 = 0.0;
+	int res = 0;
+
+	res = simple_linear_regression(HouseArrayx, HouseArray, sizeof(HouseArrayx) / sizeof(HouseArrayx[0]), &slope, &intercept, &r2);
+	HouseModel.Alpha = slope;
+	HouseModel.Beta = intercept;
+
+}
+
+double HPPowerSetpoint(double AmbTemp) {
+	// Function that calculates the temperature setpoint for the heatpump
+	// Based on HouseCurve, added BetaMod to shift curve vertically
+	double Setpoint;
+
+	Setpoint = HouseModel.Alpha * AmbTemp + HouseModel.Beta + HouseModel.BetaMod; // TBD temperature + power??
+	return Setpoint;
+}
+
+void BetaAdjust(void) {
+	// Funcion determines of the house curve needs to be shifted to regulate the heat pump temperature
+#define BetaStep 0.5
+
+	if (KValgo[0].kv_UFH > KValgo[0].kv_target_UFH) {
+		// Increase by a defined step
+		HouseModel.Beta += BetaStep;
+	}
+	else {
+		// Decrease by a defined step
+		HouseModel.Beta -= BetaStep;
+	}
+
+}
+
---
+++ KVAlgoritm-Update/Source.cpp
@@ -0,0 +1,285 @@
+//KVtargetAlgoritm(CurrentKVData, 0); // every 30 minutes
+
+// sensor readings every second (except when changing zone)
+// pump head, flow, temperatures
+// No power from HP
+
+//#include "debugapi.h"
+#include <iostream>
+
+#include <fstream>
+#include <string.h>
+#include "string"
+
+#include "AlgoFunctions.h"
+
+int counter;
+int i;
+float value1;
+
+using namespace std;
+
+double KVmeas(long index) {
+	// Function emulates the KV measurement
+	// gets the KV value at a certain index
+	return (KVarrayData[index]);
+}
+
+void LoadKVdatafromFile(void) {
+	// Get KV data from file and load into array
+	ifstream fin("KV_EmulationNom_Mod_DK.csv");
+	string strline, strremain, strCol1, strout;
+
+	string delimeter = ";";
+
+	double KVtmpvar1, KVtmpvar2;
+
+	char tmpstr[100];
+	char* strptr;
+
+	long LineNo;
+	int d1;
+
+	LineNo = 0;
+	while (!fin.eof()) {
+		getline(fin, strline, '\n'); // get line
+		d1 = static_cast<int>(strline.find(';')); // find delimiter
+
+		strCol1 = strline.substr(0, d1); // parse first Column
+		d1++;
+		strremain = strline.substr(d1); // remaining line
+
+		//std::cout << "LineNo:" << LineNo << "\n";
+		//std::cout << strCol1 << "\n";
+		//std::cout << strremain << "\n";
+
+		//std::string num = "0.6";
+
+		// Store in array
+		KVtmpvar1 = ::atof(strCol1.c_str());
+		KVtmpvar2 = ::atof(strremain.c_str());
+
+		KVarrayData[LineNo] = KVtmpvar2;
+
+		LineNo++;
+
+		//if (LineNo >= 1000) break;
+		if (LineNo > KV_ARRAY_SIZE-1) break;
+		
+	}
+	fin.close();
+}
+
+void stdoutcheck(void) {
+
+	counter = 1;
+
+	std::cout << "Optimizer\n";
+
+	ifstream fin("Infile.csv");
+	ofstream fout("OutFile.csv");
+	string strline, strremain, strCol1, strout;
+
+	string delimeter = ";";
+
+	char tmpstr[100];
+	char* strptr;
+
+	int LineNo;
+	int d1;
+	double Outdoor, EnergyCost;
+
+	typedef struct {
+		double Outdoor;
+		double EnergyCost;
+	} ForecastValuesStruct;
+
+	ForecastValuesStruct ForecastValues[100]; // forecast values for 24 hours
+	strptr = tmpstr;
+
+	LineNo = 0;
+	while (!fin.eof()) {
+		getline(fin, strline, '\n'); // get line
+		d1 = static_cast<int>(strline.find(';')); // find dilimiter
+
+		strCol1 = strline.substr(0, d1); // parse first Column
+		d1++;
+		strremain = strline.substr(d1); // remaining line
+
+		std::cout << "LineNo:" << LineNo << "\n";
+		std::cout << strCol1 << "\n";
+		std::cout << strremain << "\n";
+
+		std::string num = "0.6";
+
+		Outdoor = ::atof(strCol1.c_str());
+		EnergyCost = ::atof(strremain.c_str());
+
+		ForecastValues[LineNo].Outdoor = Outdoor;
+		ForecastValues[LineNo].EnergyCost = EnergyCost;
+
+		//Outdoor = strtof(strCol1, NULL);
+
+		//strcpy(strptr, strCol1);
+
+		//value1 = strtof(strptr, NULL);
+
+		strout.append(strCol1);
+		strout.append(delimeter);
+
+		fout << strout << endl; //out file line
+		LineNo++;
+	}
+
+	fin.close();
+	fout.close();
+
+	for (i = 0; i < 10; i++) {
+		counter = counter * 2;
+		counter = counter * 2;
+		std::cout << "counter" << counter << "\n";
+	}
+
+}
+
+void MoveKVarray(void) {
+	// Function moves the entire array one tick (so [0] is free for new data)
+	// Bad efficiency, but easier for debugging right now
+	long i;
+
+	for (i = KV_ARRAY_SIZE-1; i > 0; i--) {
+		// Move one pos
+		KValgo[i].kv_UFH = KValgo[i-1].kv_UFH;
+		KValgo[i].kv_mean_UFH = KValgo[i-1].kv_mean_UFH;
+		KValgo[i].kv_high_UFH = KValgo[i-1].kv_high_UFH;
+		KValgo[i].kv_low_UFH = KValgo[i-1].kv_low_UFH;
+		KValgo[i].kv_target_UFH = KValgo[i-1].kv_target_UFH;
+	}
+}
+
+void PrintKVArray(void) {
+	// Function prints KV array
+	long i;
+	double test1;
+
+	ofstream fout("KVOutFile.csv");
+	string strline, strremain, strCol1, strout;
+
+	string delimeter = ",";
+
+	char tmpstr[100];
+	char* strptr;
+	strptr = tmpstr;
+	test1 = 11.1;
+
+	//fout << "ArrayIndex; KV_UFH; KV_MEAN_UFH; KV_HIGH_UFH; KV_LOW_UFH; KV_TARGET_UFH" << "\n";
+
+
+	for (i = 0; i < KV_ARRAY_SIZE; i++) {
+		//strout.append(strCol1);
+		//strout.append(delimeter);
+		//KValgo[i].kv_mean_UFH = KValgo[i - 1].kv_mean_UFH;
+		//KValgo[i].kv_high_UFH = KValgo[i - 1].kv_high_UFH;
+		//KValgo[i].kv_low_UFH = KValgo[i - 1].kv_low_UFH;
+		//KValgo[i].kv_target_UFH = KValgo[i - 1].kv_target_UFH;
+
+		fout << i << delimeter << KValgo[i].kv_UFH << delimeter << KValgo[i].kv_mean_UFH << delimeter << KValgo[i].kv_high_UFH << delimeter << KValgo[i].kv_low_UFH << delimeter << KValgo[i].kv_target_UFH << delimeter << "\n";
+
+
+	}
+	
+	//fout << strout << endl; //out file line
+	fout << endl; //out file line
+
+	fout.close();
+		
+}
+
+void PrintHeatCurve(void) {
+	// Function prints Heat Curve array
+	long i;
+	double test1;
+
+	ofstream fout("HeatOutFile.csv");
+	string strline, strremain, strCol1, strout;
+
+	string delimeter = ",";
+
+	char tmpstr[100];
+	char* strptr;
+	strptr = tmpstr;
+	test1 = 11.1;
+
+	fout << "ArrayIndex" << delimeter << "Index" << delimeter << "Xvalues" << delimeter << "Power" << "\n";
+
+
+	for (i = 0; i < HOUSE_ARRAY_SIZE; i++) {
+		fout << i << delimeter << HouseArrayx[i] << delimeter << HouseArray[i] << delimeter << "\n";
+	}
+	fout << endl; //out file line
+
+	fout.close();
+}
+
+//int main(void) {
+//	cout<<"Hello World\n"<<test();
+//}
+
+int main(void) {
+
+	int i;
+	long timestamptick;
+	double CurrentKVData;
+	double AmbientTemp;
+	//stdoutcheck();
+
+	test();
+
+	// Load KV data for test period into array
+	LoadKVdatafromFile();
+	AmbientTemp = 20;
+	// Emulate that time is ticking by
+	// We are running in House mode only
+	SystemMode = ONLYHEATING;
+
+	// run the KValgorimn from the start to end of the KVarray data
+	timestamptick = 0;
+
+	while (timestamptick < 10000) {
+		// Get data from KV measurement
+		CurrentKVData = KVmeas(timestamptick);
+		// Move data one tick (data free at [0])
+		MoveKVarray();
+		// run KV algoritm
+		// Copy current KV data into [0] position
+		KVtargetAlgoritm(CurrentKVData, 0); // every 30 minutes
+
+		// Update HouseModel
+		if (SystemMode == ONLYHEATING) {
+			// Allowed to update HouseModel since only HP is heating house (TBD what about delays?)
+			UpdateHeatCurve(AmbientTemp, CurrentKVData);
+			// Adjust temperature
+			BetaAdjust();
+			// Run update of heat pump
+			HPPowerSetpoint(AmbientTemp);
+		}
+		timestamptick++;
+	}
+
+	// Print array
+	PrintKVArray();
+	timestamptick = 0;
+
+	// Load default heat curve
+	LoadDefaultHeatCurve();
+
+	// Print heat curve
+	PrintHeatCurve();
+
+	// endless loop
+	while (1) {
+		// Run temperature setpoint
+	}
+
+	i = 0;
+}
